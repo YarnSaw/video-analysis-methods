ea444cf5416a587f92d17aaf99d1a459a1d7f57d
Ryan Saweczko <yarn.sawe@gmail.com>
2023-01-18 16:07:42 -0500

    add file to separate subset further to only include videos of the same dimensions

--- separate_data_same_size.py
+++ separate_data_same_size.py
@@ -1,40 +1,51 @@
 import os, json, shutil, csv, re, cv2
 
 
-def findIDsForTags(tags, fileName):
+### Take from the 'subset' folder and make a 'subset2' folder that has all videos be the same dimensions
+
+def findIDsForTags(direct, tags, fileName):
   labels = []
-  with open(f'labels/{fileName}') as file:
+  with open(f'subset/{fileName}') as file:
     labels = json.load(file)
   
   ids = []
   rows = []
   for label in labels:
       if label['template'] in tags:
-        ids.append(label['id'])
-        rows.append(label)
-
-  with open (f'subset-{fileName}', 'w+') as file:
+        video = cv2.VideoCapture(f'subset/data/{label["id"]}.webm')
+        if video.get(3) == 427 and video.get(4) == 240:
+          ids.append(label['id'])
+          rows.append(label)
+  print(f'{direct}/{fileName}')
+  with open (f'{direct}/{fileName}', 'w+') as file:
     json.dump(rows, file, indent=1)
   return ids
 
 def copyIDsToNewDirectory(directory, ids):
-  try:
-    os.mkdir(directory)
-  except:
-    pass # directory already exists
   for id in ids:
-    shutil.copy(f'20bn-something-something-v2/{id}.webm', f'{directory}/{id}.webm')
+    shutil.copy(f'subset/data/{id}.webm', f'{directory}/data/{id}.webm')
 
 
 if __name__ == "__main__":
   tags = ['Pushing [something] from left to right', 'Putting [something] on a surface', 'Uncovering [something]']
   direct = "subset2"
-  file = 'train.json'
-  ids = findIDsForTags(tags, file)
+
+  try:
+    os.mkdir(direct)
+  except:
+    pass # directory already exists
+  try:
+    os.mkdir(direct+'/data')
+  except:
+    pass # directory already exists
+
+
+  file = 'subset-train.json'
+  ids = findIDsForTags(direct, tags, file)
   copyIDsToNewDirectory(direct, ids)
 
-  file = 'validation.json'
-  ids = findIDsForTags(tags, file)
+  file = 'subset-validation.json'
+  ids = findIDsForTags(direct, tags, file)
   copyIDsToNewDirectory(direct, ids)
 
 
@@ -43,7 +54,7 @@ if __name__ == "__main__":
   ids = [];
   testJson = [];
   newCSV = [];
-  with open('labels/test-answers.csv') as file:
+  with open('subset/subset-test-answers.csv') as file:
     reader = csv.reader(file, delimiter=";")
     for row in reader:
       if re.sub(r'something', r'[something]', row[1]) in tags: # this one needs the square braces around something so I don't need to redefine tags - being lazy = more work
@@ -51,9 +62,9 @@ if __name__ == "__main__":
         testJson.append({"id":f"{row[0]}"})
         newCSV.append(row);
   copyIDsToNewDirectory(direct, ids)
-  with open ('subset-test.json', 'w+') as file:
+  with open (f'{direct}/subset-test.json', 'w+') as file:
     json.dump(testJson, file, indent=1)
-  with open ('subset-test-answers.csv', 'w+') as file:
+  with open (f'{direct}/subset-test-answers.csv', 'w+') as file:
     writer = csv.writer(file, delimiter=';')
     for row in newCSV:
       writer.writerow(row);
